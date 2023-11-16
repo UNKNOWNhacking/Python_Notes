a8240acd3edb8cd14a66d0d1389395a817d0ea78
UNKNOWNhacking <114736256+UNKNOWNhacking@users.noreply.github.com>
2023-11-16 08:59:37 +0530

    Delete Problem Solving Python Programming/Unit 1 directory

--- Problem Solving Python Programming/Unit 1/insertacard.py
+++
@@ -1,7 +0,0 @@
-import bisect
-def insert(list, n):
-    bisect.insort(list, n) 
-    return list
-list = [1, 2, 4]
-n = 3
-print(insert(list, n))
--- Problem Solving Python Programming/Unit 1/towerofhonai.py
+++
@@ -1,8 +0,0 @@
-def TowerOfHonai(n, from_rod, to_rod, aux_rod):
-    if n == 0:
-        return
-    TowerOfHonai(n-1, from_rod, aux_rod, to_rod)
-    print("Move disk", n, "from rod", from_rod, "to rod", to_rod)
-    TowerOfHonai(n-1, aux_rod, to_rod, from_rod)
-n = 3
-TowerOfHonai(n, "A", "C", "B")

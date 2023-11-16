64b7cb17e1cd2c704f3b6606651de7259ff6c400
UNKNOWNhacking <114736256+UNKNOWNhacking@users.noreply.github.com>
2023-11-16 09:10:33 +0530

    Add files via upload

---
+++ even_number.py
@@ -0,0 +1,12 @@
+def evensum(n):
+    curr = 2
+    sum = 0
+    i = 1
+    while i <= n:
+        sum += curr
+        curr += 2
+        i = i + 1
+    return sum
+n = 20
+print("sum of first ", n, "even number is: ",evensum(n))
+ 
